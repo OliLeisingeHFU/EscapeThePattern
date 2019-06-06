4f48980d7cba99a62a378067fcd77952b3390bda
OliverHFU <oliver.leisinger@hs-furtwangen.de>
2019-06-06 16:26:19 +0200

    Added Stift

--- Escape The Pattern/Assets/Scripts/ItemPickUp.cs
+++ Escape The Pattern/Assets/Scripts/ItemPickUp.cs
@@ -10,6 +10,8 @@ public class ItemPickUp : MonoBehaviour
         
     }
 
+    this.getComponent<Meshrenderer>().enabled = false;
+
     // Update is called once per frame
     void Update()
     {
